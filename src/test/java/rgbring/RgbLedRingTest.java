37c2dfb3e098d9856e858c770930dde95720561f
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-11-02 21:43:12 +0100

    autocommit on change

--- src/test/java/rgbring/RgbLedRingTest.java
+++ src/test/java/rgbring/RgbLedRingTest.java
@@ -21,8 +21,206 @@ import org.junit.Test;
 
 public class RgbLedRingTest {
 
+	private final class IMqttClientForTest implements IMqttClient {
+		public void unsubscribe(String[] topicFilters) throws MqttException {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void unsubscribe(String topicFilter) throws MqttException {
+			// TODO Auto-generated method stub
+
+		}
+
+		public IMqttToken subscribeWithResponse(String[] topicFilters, int[] qos,
+				IMqttMessageListener[] messageListeners) throws MqttException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public IMqttToken subscribeWithResponse(String topicFilter, int qos,
+				IMqttMessageListener messageListener) throws MqttException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public IMqttToken subscribeWithResponse(String[] topicFilters, int[] qos) throws MqttException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public IMqttToken subscribeWithResponse(String[] topicFilters, IMqttMessageListener[] messageListeners)
+				throws MqttException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public IMqttToken subscribeWithResponse(String topicFilter, int qos) throws MqttException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public IMqttToken subscribeWithResponse(String topicFilter, IMqttMessageListener messageListener)
+				throws MqttException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public IMqttToken subscribeWithResponse(String[] topicFilters) throws MqttException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public IMqttToken subscribeWithResponse(String topicFilter) throws MqttException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public void subscribe(String[] topicFilters, int[] qos, IMqttMessageListener[] messageListeners)
+				throws MqttException {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void subscribe(String topicFilter, int qos, IMqttMessageListener messageListener)
+				throws MqttException {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void subscribe(String[] topicFilters, IMqttMessageListener[] messageListeners)
+				throws MqttException {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void subscribe(String topicFilter, IMqttMessageListener messageListener)
+				throws MqttException, MqttSecurityException {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void subscribe(String[] topicFilters, int[] qos) throws MqttException {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void subscribe(String topicFilter, int qos) throws MqttException {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void subscribe(String[] topicFilters) throws MqttException {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void subscribe(String topicFilter) throws MqttException, MqttSecurityException {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void setManualAcks(boolean manualAcks) {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void setCallback(MqttCallback callback) {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void publish(String topic, byte[] payload, int qos, boolean retained)
+				throws MqttException, MqttPersistenceException {
+			messageToSend.add(new String(payload));
+
+		}
+
+		public void publish(String topic, MqttMessage message) throws MqttException, MqttPersistenceException {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void messageArrivedComplete(int messageId, int qos) throws MqttException {
+			// TODO Auto-generated method stub
+
+		}
+
+		public boolean isConnected() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		public MqttTopic getTopic(String topic) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public String getServerURI() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public IMqttDeliveryToken[] getPendingDeliveryTokens() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public String getClientId() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public void disconnectForcibly(long quiesceTimeout, long disconnectTimeout) throws MqttException {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void disconnectForcibly(long disconnectTimeout) throws MqttException {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void disconnectForcibly() throws MqttException {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void disconnect(long quiesceTimeout) throws MqttException {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void disconnect() throws MqttException {
+			// TODO Auto-generated method stub
+
+		}
+
+		public IMqttToken connectWithResult(MqttConnectOptions options)
+				throws MqttSecurityException, MqttException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public void connect(MqttConnectOptions options) throws MqttSecurityException, MqttException {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void connect() throws MqttSecurityException, MqttException {
+			// TODO Auto-generated method stub
+
+		}
+
+		public void close() throws MqttException {
+			// TODO Auto-generated method stub
+
+		}
+	}
+
 	private RgbLedRing ring;
 	private IMqttClient client;
+	private final List<String> messageToSend = new ArrayList<String>();
 
 	@Test
 	public void givenRingWith2LedsShouldEnlightNoLedWhenLevelIsZero() {
@@ -92,210 +290,13 @@ public class RgbLedRingTest {
 		for (int i = 0; i < ring.size(); i++) {
 			assertThat(ring.getLed(i), is(states[i]));
 
-			final List<String> messageToSend = new ArrayList<String>();
 
-			client = new IMqttClient() {
-
-				public void unsubscribe(String[] topicFilters) throws MqttException {
-					// TODO Auto-generated method stub
-
-				}
-
-				public void unsubscribe(String topicFilter) throws MqttException {
-					// TODO Auto-generated method stub
-
-				}
-
-				public IMqttToken subscribeWithResponse(String[] topicFilters, int[] qos,
-						IMqttMessageListener[] messageListeners) throws MqttException {
-					// TODO Auto-generated method stub
-					return null;
-				}
-
-				public IMqttToken subscribeWithResponse(String topicFilter, int qos,
-						IMqttMessageListener messageListener) throws MqttException {
-					// TODO Auto-generated method stub
-					return null;
-				}
-
-				public IMqttToken subscribeWithResponse(String[] topicFilters, int[] qos) throws MqttException {
-					// TODO Auto-generated method stub
-					return null;
-				}
-
-				public IMqttToken subscribeWithResponse(String[] topicFilters, IMqttMessageListener[] messageListeners)
-						throws MqttException {
-					// TODO Auto-generated method stub
-					return null;
-				}
-
-				public IMqttToken subscribeWithResponse(String topicFilter, int qos) throws MqttException {
-					// TODO Auto-generated method stub
-					return null;
-				}
-
-				public IMqttToken subscribeWithResponse(String topicFilter, IMqttMessageListener messageListener)
-						throws MqttException {
-					// TODO Auto-generated method stub
-					return null;
-				}
-
-				public IMqttToken subscribeWithResponse(String[] topicFilters) throws MqttException {
-					// TODO Auto-generated method stub
-					return null;
-				}
-
-				public IMqttToken subscribeWithResponse(String topicFilter) throws MqttException {
-					// TODO Auto-generated method stub
-					return null;
-				}
-
-				public void subscribe(String[] topicFilters, int[] qos, IMqttMessageListener[] messageListeners)
-						throws MqttException {
-					// TODO Auto-generated method stub
-
-				}
-
-				public void subscribe(String topicFilter, int qos, IMqttMessageListener messageListener)
-						throws MqttException {
-					// TODO Auto-generated method stub
-
-				}
-
-				public void subscribe(String[] topicFilters, IMqttMessageListener[] messageListeners)
-						throws MqttException {
-					// TODO Auto-generated method stub
-
-				}
-
-				public void subscribe(String topicFilter, IMqttMessageListener messageListener)
-						throws MqttException, MqttSecurityException {
-					// TODO Auto-generated method stub
-
-				}
-
-				public void subscribe(String[] topicFilters, int[] qos) throws MqttException {
-					// TODO Auto-generated method stub
-
-				}
-
-				public void subscribe(String topicFilter, int qos) throws MqttException {
-					// TODO Auto-generated method stub
-
-				}
-
-				public void subscribe(String[] topicFilters) throws MqttException {
-					// TODO Auto-generated method stub
-
-				}
-
-				public void subscribe(String topicFilter) throws MqttException, MqttSecurityException {
-					// TODO Auto-generated method stub
-
-				}
-
-				public void setManualAcks(boolean manualAcks) {
-					// TODO Auto-generated method stub
-
-				}
-
-				public void setCallback(MqttCallback callback) {
-					// TODO Auto-generated method stub
-
-				}
-
-				public void publish(String topic, byte[] payload, int qos, boolean retained)
-						throws MqttException, MqttPersistenceException {
-					messageToSend.add(new String(payload));
-
-				}
-
-				public void publish(String topic, MqttMessage message) throws MqttException, MqttPersistenceException {
-					// TODO Auto-generated method stub
-
-				}
-
-				public void messageArrivedComplete(int messageId, int qos) throws MqttException {
-					// TODO Auto-generated method stub
-
-				}
-
-				public boolean isConnected() {
-					// TODO Auto-generated method stub
-					return false;
-				}
-
-				public MqttTopic getTopic(String topic) {
-					// TODO Auto-generated method stub
-					return null;
-				}
-
-				public String getServerURI() {
-					// TODO Auto-generated method stub
-					return null;
-				}
-
-				public IMqttDeliveryToken[] getPendingDeliveryTokens() {
-					// TODO Auto-generated method stub
-					return null;
-				}
-
-				public String getClientId() {
-					// TODO Auto-generated method stub
-					return null;
-				}
-
-				public void disconnectForcibly(long quiesceTimeout, long disconnectTimeout) throws MqttException {
-					// TODO Auto-generated method stub
-
-				}
-
-				public void disconnectForcibly(long disconnectTimeout) throws MqttException {
-					// TODO Auto-generated method stub
-
-				}
-
-				public void disconnectForcibly() throws MqttException {
-					// TODO Auto-generated method stub
-
-				}
-
-				public void disconnect(long quiesceTimeout) throws MqttException {
-					// TODO Auto-generated method stub
-
-				}
-
-				public void disconnect() throws MqttException {
-					// TODO Auto-generated method stub
-
-				}
-
-				public IMqttToken connectWithResult(MqttConnectOptions options)
-						throws MqttSecurityException, MqttException {
-					// TODO Auto-generated method stub
-					return null;
-				}
-
-				public void connect(MqttConnectOptions options) throws MqttSecurityException, MqttException {
-					// TODO Auto-generated method stub
-
-				}
-
-				public void connect() throws MqttSecurityException, MqttException {
-					// TODO Auto-generated method stub
-
-				}
-
-				public void close() throws MqttException {
-					// TODO Auto-generated method stub
-
-				}
-			};
 			assertThat(messageToSend.get(0), is("#ffffff"));
 		}
 	}
 
 	private void givenLeds(int ledCount) {
+		client = new IMqttClientForTest();
 		this.ring = new RgbLedRing(ledCount) {
 			@Override
 			void setLevel(int level) {
