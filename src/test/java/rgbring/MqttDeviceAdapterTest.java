5b0b68541062a4e178313ab09b7493a0c7533026
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-11-03 00:05:27 +0100

    autocommit on change

--- src/test/java/rgbring/MqttDeviceAdapterTest.java
+++ src/test/java/rgbring/MqttDeviceAdapterTest.java
@@ -35,11 +35,15 @@ public class MqttDeviceAdapterTest {
 
 		TopicAndMessage received;
 		do {
-			received = received;
+			received = receivingCLientHasReceived();
 		} while (received == null);
 		assertThat(received.getTopic(), is("someLed/rgb/42"));
 		assertThat(received.getPayload(), is("#123456"));
 
+	}
+
+	private TopicAndMessage receivingCLientHasReceived() {
+		return received;
 	}
 
 	private void createReceiver() throws MqttException, MqttSecurityException {
