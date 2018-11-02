c2c66653d0c5d4d8a55b1a98706bbb1a4374b8c6
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-11-02 22:11:21 +0100

    autocommit on change

--- src/test/java/rgbring/RgbLedRing.java
+++ src/test/java/rgbring/RgbLedRing.java
@@ -5,14 +5,11 @@ import org.eclipse.paho.client.mqttv3.IMqttClient;
 public class RgbLedRing {
 
 	private static final int MAX_LEVEL = 100;
-	@Deprecated
-	public IMqttClient mqttClient;
 	private DeviceAdapter deviceAdapter;
 	private int ledCount;
 
 	public RgbLedRing(int ledCount, DeviceAdapter deviceAdapter) {
 		this.ledCount = ledCount;
-		this.mqttClient = mqttClient;
 		this.deviceAdapter = deviceAdapter;
 	}
 
