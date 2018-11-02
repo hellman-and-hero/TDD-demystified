35940428a00bb8e5cc2c04d67e7a236379da0608
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-11-02 22:03:11 +0100

    autocommit on change

--- src/test/java/rgbring/RgbLedRing.java
+++ src/test/java/rgbring/RgbLedRing.java
@@ -21,11 +21,11 @@ public class RgbLedRing {
 		}
 	}
 
-	private void setLed(int level, int i) {
-		boolean led = level > 100 / size() * i;
+	private void setLed(int level, int ledNum) {
+		boolean led = level > 100 / size() * ledNum;
 		try {
 			String payload = led ? "#ffffff" : "#000000";
-			mqttClient.publish("someLed/rgb/" + i, new MqttMessage(payload.getBytes()));
+			mqttClient.publish("someLed/rgb/" + ledNum, new MqttMessage(payload.getBytes()));
 		} catch (MqttPersistenceException e) {
 			throw new RuntimeException(e);
 		} catch (MqttException e) {
