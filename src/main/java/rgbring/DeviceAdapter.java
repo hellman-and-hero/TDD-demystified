e55da34b82e7e4ab50ab45f4a7b791f99128d666
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-11-02 22:07:55 +0100

    autocommit on change

--- src/main/java/rgbring/DeviceAdapter.java
+++ src/main/java/rgbring/DeviceAdapter.java
@@ -1,5 +1,14 @@
 package rgbring;
 
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+
 public class DeviceAdapter {
+
+	private IMqttClient mqttClient;
+
+	public DeviceAdapter(IMqttClient mqttClient) {
+		this.mqttClient = mqttClient;
+		// TODO Auto-generated constructor stub
+	}
 
 }
