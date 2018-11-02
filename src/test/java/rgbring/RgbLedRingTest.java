ba5fb2955689c80ed2e004adb4c6929ab1245027
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-11-02 21:02:56 +0100

    autocommit on change

--- src/test/java/rgbring/RgbLedRingTest.java
+++ src/test/java/rgbring/RgbLedRingTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 
 public class RgbLedRingTest {
 
-	private boolean[] leds ;
+	private boolean[] leds;
 
 	@Test
 	public void givenRingWith2LedsShouldEnlightNoLedWhenLevelIsZero() {
