c616091011858bf8eb027c4907fafac823023de9
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-11-02 23:36:50 +0100

    autocommit on change

---
+++ src/test/java/rgbring/MqttDeviceAdapterTest.java
@@ -0,0 +1,14 @@
+package rgbring;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MqttDeviceAdapterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
