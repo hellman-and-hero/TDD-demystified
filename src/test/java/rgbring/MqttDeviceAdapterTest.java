44551e263bf8c5002a794c0c6813cddad3b5fc0e
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-11-02 23:37:52 +0100

    autocommit on change

--- src/test/java/rgbring/MqttDeviceAdapterTest.java
+++ src/test/java/rgbring/MqttDeviceAdapterTest.java
@@ -1,13 +1,18 @@
 package rgbring;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
 import org.junit.Test;
 
 public class MqttDeviceAdapterTest {
 
 	@Test
-	public void test() {
+	public void test() throws MqttException {
+		IMqttClient client = new MqttClient("localhost", "someledclient");
+		MqttDeviceAdapter sut = new MqttDeviceAdapter(client);
 		fail("Not yet implemented");
 	}
 
