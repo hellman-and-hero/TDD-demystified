2924fd0c5e1d416aaa3f1d72eb6d9080ce07c9a6
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-11-02 23:54:18 +0100

    autocommit on change

--- src/test/java/rgbring/MqttDeviceAdapterTest.java
+++ src/test/java/rgbring/MqttDeviceAdapterTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.fail;
 import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttSecurityException;
 import org.junit.Test;
 
 public class MqttDeviceAdapterTest {
@@ -14,11 +15,15 @@ public class MqttDeviceAdapterTest {
 		// TODO do not depend on eclipse infrastructure
 		IMqttClient client = new MqttClient("tcp://iot.eclipse.org", "someledclient");
 		client.connect();
+		createReceiver();
+		MqttDeviceAdapter sut = new MqttDeviceAdapter(client);
+
+	}
+
+	private void createReceiver() throws MqttException, MqttSecurityException {
 		IMqttClient receivingClient = new MqttClient("tcp://iot.eclipse.org", "someOtherClient");
 		receivingClient.connect();
 		receivingClient.subscribe("someLed/rgb/#");
-		MqttDeviceAdapter sut = new MqttDeviceAdapter(client);
-
 	}
 
 }
