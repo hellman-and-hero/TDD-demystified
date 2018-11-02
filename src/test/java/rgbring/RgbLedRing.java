a4591968bfc6846c1e6f68b2fb01230f128cc142
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-11-02 21:46:55 +0100

    autocommit on change

--- src/test/java/rgbring/RgbLedRing.java
+++ src/test/java/rgbring/RgbLedRing.java
@@ -1,18 +1,30 @@
 package rgbring;
 
 import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
 
 public class RgbLedRing {
 	
 	private final boolean[] leds;
+	private IMqttClient mqttClient;
 
 	public RgbLedRing(int ledCount, IMqttClient mqttClient) {
+		this.mqttClient = mqttClient;
 		this.leds = new boolean[ledCount];
 	}
 
 	void setLevel(int level) {
 		for (int i = 0; i < leds.length; i++) {
 			leds[i] = level > 100 / leds.length * i;
+			try {
+				client.publish("someLed/rgb/" + "0", new MqttMessage("#ffffff".getBytes()));
+			} catch (MqttPersistenceException e) {
+				throw new RuntimeException(e);
+			} catch (MqttException e) {
+				throw new RuntimeException(e);
+			}
 		}
 	}
 
