82e5eca0c8ee24eabfe5c501c52f70bb1e943f55
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2018-11-03 01:10:17 +0100

    autocommit on change

--- src/main/java/rgbring/RgbLedRing.java
+++ src/main/java/rgbring/RgbLedRing.java
@@ -19,7 +19,7 @@ public class RgbLedRing {
 
 	void setLevel(int level) {
 		for (int i = 0; i < size(); i++) {
-			int value = MAX_LEVEL / size() * i;
+			int value = (int) (MAX_LEVEL / size() * i);
 			System.out.println(value);
 			boolean ledState = level > value;
 			String color = ledState ? determineColor(i) : OFF;
